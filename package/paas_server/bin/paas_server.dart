808ff4caac3a26ee67e9d7621a959bd6d0baefec
Your Name <you@example.com>
2023-11-23 18:54:37 +0700

    update

--- package/paas_server/bin/paas_server.dart
+++ package/paas_server/bin/paas_server.dart
@@ -1,5 +1,23 @@
 import 'package:paas_server/paas_server.dart' as paas_server;
+import 'package:telegram_client/alfred/alfred_io.dart';
+import 'package:universal_io/io.dart';
 
-void main(List<String> arguments) {
+void main(List<String> arguments) async {
+  Alfred app = Alfred();
+
+  String host = Platform.environment["HOST"] ?? "0.0.0.0";
+  int port = int.tryParse(Platform.environment["PORT"] ?? "") ?? 8097;
+
+  app.all("/", (req, res) {
+
+  });
   
+  app.all("/api", (req, res) {
+    
+  });
+
+  await app.listen(
+    port,
+    host,
+  );
 }
